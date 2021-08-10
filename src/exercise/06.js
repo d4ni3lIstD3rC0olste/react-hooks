4febdcd7056d05dddf0d2bef640afc58e5e92fc8
Daniel <daniel.ritter@reservix.de>
2021-08-10 09:32:17 +0200

    Exercise 06

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,15 +1,38 @@
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import * as React from 'react';
+//import { number } from 'yargs';
+import { fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm } from '../pokemon';
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    setPokemon(null);
+    setError(null);
+
+    fetchPokemon(pokemonName).then(
+      pokemonData => setPokemon(pokemonData), 
+      error => setError(error)
+    );
+  }, [pokemonName]);
+
+  return (
+    <div>
+      {error ? 
+      <div role="alert">
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+      : (!pokemonName ? 'Submit a pokemon' : (!pokemon ? <PokemonInfoFallback name={pokemonName} /> : <PokemonDataView pokemon={pokemon} />))}
+    </div>
+    
+  );
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
@@ -23,9 +46,6 @@ function PokemonInfo({pokemonName}) {
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
-
-  // 💣 remove this
-  return 'TODO'
 }
 
 function App() {
